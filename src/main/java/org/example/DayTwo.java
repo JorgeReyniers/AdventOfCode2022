5e980c1d4b586fe8189c88979ab70805c68fef4a
jreyniers <jorge.reyniers@hotmail.com>
2022-12-06 13:35:32 +0100

    Setup Day Two

---
+++ src/main/java/org/example/DayTwo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class DayTwo {
+    public DayTwo() {
+    }
+
+    public int execute(ArrayList<String> inputDayTwo) {
+        return 0;
+    }
+}
---
+++ src/test/java/DayTwoTest.java
@@ -0,0 +1,23 @@
+import org.example.DayOne;
+import org.example.DayTwo;
+import org.example.Utils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DayTwoTest {
+
+    @Test
+    void DayTwoTest_HappyFlow() throws IOException, URISyntaxException {
+        //arrange
+        DayTwo dayTwo = new DayTwo();
+        Utils utils = new Utils();
+        ArrayList<String> strategyGuide = utils.ReadFile("testDayTwo.txt");
+        int totalScore = dayTwo.execute(strategyGuide);
+        Assertions.assertEquals(15, totalScore);
+    }
+}
