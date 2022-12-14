c79b6f4591b5c15aaa1498de1f3f1797ceefda29
jreyniers <jorge.reyniers@hotmail.com>
2022-12-14 10:24:55 +0100

    Implement Day Six Part One

--- src/main/java/org/example/DaySix.java
+++ src/main/java/org/example/DaySix.java
@@ -1,11 +1,20 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DaySix implements Day {
     @Override
     public int execute(ArrayList<String> input) {
-        return 0;
+        String signal = input.get(0);
+        String[] symbols = signal.split("");
+        int from = 0;
+        String[] subset = Arrays.copyOfRange(symbols, from, from+4);
+        while (Arrays.stream(subset).distinct().count() != subset.length) {
+            from++;
+            subset = Arrays.copyOfRange(symbols, from, from+4);
+        }
+        return from+4;
     }
 
     @Override
