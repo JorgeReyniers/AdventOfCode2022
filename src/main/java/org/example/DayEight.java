613e2f1411c28f3602d5adb45d3bf77b23a9ea28
jreyniers <jorge.reyniers@hotmail.com>
2022-12-16 07:23:14 +0100

    Implement Day Eight Part One - count trees at the edge

--- src/main/java/org/example/DayEight.java
+++ src/main/java/org/example/DayEight.java
@@ -1,10 +1,36 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DayEight implements Day{
     @Override
     public int execute(ArrayList<String> input) {
-        return 0;
+        int[][] treeMap = new int[input.size()][input.get(0).length()];
+        fillTreeMap(input, treeMap);
+        System.out.println(Arrays.deepToString(treeMap));
+        return countVisibleTrees(treeMap);
+    }
+
+    private int countVisibleTrees(int[][] treeMap) {
+        int visibleTrees = 0;
+        for (int i = 0 ; i < treeMap.length ; i++) {
+            for (int j = 0 ; j < treeMap[i].length ; j++) {
+                if (i == 0 || i == treeMap.length - 1 || j == 0 || j == treeMap[i].length - 1) {
+                    visibleTrees++;
+                }
+
+            }
+        }
+        return visibleTrees;
+    }
+
+    private static void fillTreeMap(ArrayList<String> input, int[][] treeMap) {
+        for (int i = 0; i < input.size() ; i++) {
+            String[] trees = input.get(i).split("");
+            for (int j = 0 ; j < trees.length ; j++) {
+                treeMap[i][j] = Integer.parseInt(trees[j]);
+            }
+        }
     }
 }
